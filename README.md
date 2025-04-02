5a9044d7d61c7375fc88455352bec09be6156c79
Rodrigo Jesner Silva Trindade <rodrigo.trindade@itriad.org.br>
2025-04-02 11:33:35 -0400

    feat: command line options using the library CLI11

--- README.md
+++ README.md
@@ -15,10 +15,10 @@ Crypto Tool is a command-line tool made by the programmer Rodrigo Jesner to calc
 ## Usage
 
 ```bash
-$> crypto-tool -crc32 exampleFile.bin # Shows the CRC32 of the file
-$> crypto-tool -sha256 exampleFile.bin # Shows the SHA256 of the file
-$> crypto-tool -crc32 -sha256 exampleFile.bin # Shows both CRC32 and SHA256 of the file
-$> crypto-tool -gaes128 # Generation of random AES128 key
-$> crypto-tool -eaes128 c518cb5e31f8be76da0f60963a7f0b21 -o encryptedFile.bin exampleFile.bin # Encryption of file using AES128
-$> crypto-tool -daes128 c518cb5e31f8be76da0f60963a7f0b21 -o exampleFile.bin encryptedFile.bin # The reverse of the above
+$> ./crypto-tool --crc32 -f exampleFile.bin # Shows the CRC32 of the file
+$> ./crypto-tool --sha256 -f exampleFile.bin # Shows the SHA256 of the file
+$> ./crypto-tool --crc32 --sha256 -f exampleFile.bin # Shows both CRC32 and SHA256 of the file
+$> ./crypto-tool --gaes128 # Generation of random AES128 key
+$> ./crypto-tool --eaes128 c518cb5e31f8be76da0f60963a7f0b21 -f exampleFile.bin -o encryptedFile.bin # Encryption of file using AES128
+$> ./crypto-tool --daes128 c518cb5e31f8be76da0f60963a7f0b21 -f encryptedFile.bin -o exampleFile.bin # The reverse of the above
 ```
--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,10 +1,84 @@
 #include <iostream>
 #include <cstring>
+#include "../include/CLI/CLI11.hpp"
+#include "../include/crypto/hashing.hpp"
+#include <string>
 
-#include "../include/hashing.h"
+int main(int argc, char **argv) {
+    CLI::App app("Crypto-Tool by Rodrigo Jesner");
 
-int main(void) {
-    std::string message = "Hello World!";
-    std::cout << "SHA256: " << calc_sha256(message) << std::endl;
+    std::string file;
+    std::string output_file;
+    std::string encryption_key;
+    std::string decryption_key;
+    bool crc32_flag = false;
+    bool sha256_flag = false;
+    bool aes128_gen_flag = false;
+
+    app.add_option("-f,--file", file, "Input file");
+    app.add_flag("--crc32", crc32_flag, "Compute CRC32 of the file");
+    app.add_flag("--sha256", sha256_flag, "Compute SHA256 of the file");
+    app.add_flag("--gaes128", aes128_gen_flag, "Generate a random AES128 key");
+    app.add_option("--eaes128", encryption_key, "AES128 key for encryption (hex string)");
+    app.add_option("--daes128", decryption_key, "AES128 key for decryption (hex string)");
+    app.add_option("-o,--output", output_file, "Output file");
+
+    CLI11_PARSE(app, argc, argv);
+
+    if (argc == 1) {
+        std::cout << app.help("");
+        return 1;
+    }
+
+    try {
+        if(crc32_flag || sha256_flag) {
+            if (crc32_flag) {
+                if (file.empty()) {
+                    std::cerr << "File required for CRC32 computation\n";
+                    return 1;
+                }
+                std::cout << "CRC32 Flag: " << crc32_flag << " | " << "File: " << file << std::endl;
+            }
+
+            if (sha256_flag) {
+                if (file.empty()) {
+                    std::cerr << "File required for SHA256 computation\n";
+                    return 0;
+                }
+                std::cout << "SHA256 Flag: " << sha256_flag << " | " << "File: " << file << std::endl;
+            }
+        }
+        else if(aes128_gen_flag) {
+            std::cout << "Generated AES128 Key: " << "Teste" << std::endl;
+            return 1;
+        }
+        else if (!encryption_key.empty()) {
+            if (file.empty() || output_file.empty()) {
+                std::cerr << "File and output file required for encryption\n";
+                return 1;
+            }
+            std::cout << "[AES Encryption]" << std::endl;
+            std::cout << "Key: " << encryption_key << std::endl;
+            std::cout << "File: " << file << std::endl;
+            std::cout << "Output file: " << output_file  << std::endl;
+        }
+        else if (!decryption_key.empty()) {
+            if (file.empty() || output_file.empty()) {
+                std::cerr << "File and output file required for decryption\n";
+                return 1;
+            }
+            std::cout << "[AES Decryption]" << std::endl;
+            std::cout << "Key: " << decryption_key << std::endl;
+            std::cout << "File: " << file << std::endl;
+            std::cout << "Output file: " << decryption_key << std::endl;
+        } else {
+            std::cout << app.help("");
+            return 1;
+        }
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
     return 0;
 }
